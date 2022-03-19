2f53834bd4ffaafe20710c5318b5314b3e77f788
DEAIFISH <1277676309@qq.com>
2022-03-19 16:32:40 +0800

    Changes

--- src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] arg) {
-        System.out.println("Hello World!!!");
-        System.out.println("Hello World!!!");
-        System.out.println("Hello World!!!");
-        System.out.println("Hello World!!!");
-    }
-}
