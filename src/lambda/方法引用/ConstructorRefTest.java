d32d40e06f8800bff351d84d41024ca4107dbd8f
DEAIFISH <1277676309@qq.com>
2022-09-26 23:13:38 +0800

    2022/09/26

--- src/lambda/方法引用/ConstructorRefTest.java
+++ src/lambda/方法引用/ConstructorRefTest.java
@@ -9,21 +9,21 @@ import java.util.function.Supplier;
 
 /**
  * 一、构造器引用
- *      和方法引用类似，函数式接口的抽象方法的形参列表和构造器的形参列表一致。
- *      抽象方法的返回值类型即为构造器所属的类的类型
- *
+ * 和方法引用类似，函数式接口的抽象方法的形参列表和构造器的形参列表一致。
+ * 抽象方法的返回值类型即为构造器所属的类的类型
+ * <p>
  * 二、数组引用
- *     大家可以把数组看做是一个特殊的类，则写法与构造器引用一致。
- *
+ * 大家可以把数组看做是一个特殊的类，则写法与构造器引用一致。
+ * <p>
  * Created by shkstart
  */
 public class ConstructorRefTest {
-	//构造器引用
+    //构造器引用
     //Supplier中的T get()
     //Employee的空参构造器：Employee()
     @Test
-    public void test1(){
- 
+    public void test1() {
+
         Supplier<Employee> sup = new Supplier<Employee>() {
             @Override
             public Employee get() {
@@ -32,54 +32,54 @@ public class ConstructorRefTest {
         };
         System.out.println("*******************");
 
-        Supplier<Employee>  sup1 = () -> new Employee();
+        Supplier<Employee> sup1 = () -> new Employee();
         System.out.println(sup1.get());
 
         System.out.println("*******************");
 
-        Supplier<Employee>  sup2 = Employee :: new;
+        Supplier<Employee> sup2 = Employee::new;
         System.out.println(sup2.get());
     }
 
-	//Function中的R apply(T t)
+    //Function中的R apply(T t)
     @Test
-    public void test2(){
-        Function<Integer,Employee> func1 = id -> new Employee(id);
+    public void test2() {
+        Function<Integer, Employee> func1 = id -> new Employee(id);
         Employee employee = func1.apply(1001);
         System.out.println(employee);
 
         System.out.println("*******************");
 
-        Function<Integer,Employee> func2 = Employee :: new;
+        Function<Integer, Employee> func2 = Employee::new;
         Employee employee1 = func2.apply(1002);
         System.out.println(employee1);
 
     }
 
-	//BiFunction中的R apply(T t,U u)
+    //BiFunction中的R apply(T t,U u)
     @Test
-    public void test3(){
-        BiFunction<Integer,String,Employee> func1 = (id,name) -> new Employee(id,name);
-        System.out.println(func1.apply(1001,"Tom"));
+    public void test3() {
+        BiFunction<Integer, String, Employee> func1 = (id, name) -> new Employee(id, name);
+        System.out.println(func1.apply(1001, "Tom"));
 
         System.out.println("*******************");
 
-        BiFunction<Integer,String,Employee> func2 = Employee :: new;
-        System.out.println(func2.apply(1002,"Tom"));
+        BiFunction<Integer, String, Employee> func2 = Employee::new;
+        System.out.println(func2.apply(1002, "Tom"));
 
     }
 
-	//数组引用
+    //数组引用
     //Function中的R apply(T t)
     @Test
-    public void test4(){
-        Function<Integer,String[]> func1 = length -> new String[length];
+    public void test4() {
+        Function<Integer, String[]> func1 = length -> new String[length];
         String[] arr1 = func1.apply(5);
         System.out.println(Arrays.toString(arr1));
 
         System.out.println("*******************");
 
-        Function<Integer,String[]> func2 = String[] :: new;
+        Function<Integer, String[]> func2 = String[]::new;
         String[] arr2 = func2.apply(10);
         System.out.println(Arrays.toString(arr2));
 
