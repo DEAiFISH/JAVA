b5368352331d261f03bd99ce3b162d5e036a7ab9
DEAIFISH <1277676309@qq.com>
2022-12-07 18:39:33 +0800

    2022/12/07

---
+++ src/Swing_Test/src/controller/Calculator.java
@@ -0,0 +1,20 @@
+package controller;
+
+public class Calculator{
+    public double add(double x, double y) {
+        return x + y;
+    }
+
+    public double del(double x, double y) {
+        return x - y;
+    }
+
+    public double mul(double x, double y) {
+        return x * y;
+    }
+
+    public double div(double x, double y) {
+
+        return x / y;
+    }
+}
